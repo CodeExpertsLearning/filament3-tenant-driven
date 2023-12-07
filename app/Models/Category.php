f5243e7335b923010ec8dc74e772e93cc418b18f
Nanderson Castro <nandokstro@gmail.com>
2023-12-07 16:49:13 -0300

    Mods em produtos para rela m:m com Categorias e participantes categorias e relacao m:m

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,30 +2,22 @@
 
 namespace App\Models;
 
-use App\Traits\BelongsToTenantTrait;
-use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Traits\BelongsToTenantTrait;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
-class Product extends Model
+class Category extends Model
 {
     use HasFactory, BelongsToTenantTrait;
-
-    protected $guarded = [];
-
-    public function price(): Attribute
-    {
-        return new Attribute(get: fn ($attr) => $attr / 100);
-    }
 
     public function store(): BelongsTo
     {
         return $this->belongsTo(Store::class);
     }
 
-    public function categories(): BelongsToMany
+    public function products(): BelongsToMany
     {
         return $this->belongsToMany(Product::class);
     }
